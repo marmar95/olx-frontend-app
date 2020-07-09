3ad124c651ed5af81ced2ef20c307188ebcb7aa6
@mraczek <mraczek@lsisoftware.pl>
2020-07-09 16:36:08 +0200

    Add auth module

--- olx-frontend-app/src/app/services/auth/auth.service.ts
+++ olx-frontend-app/src/app/services/auth/auth.service.ts
@@ -1,13 +1,31 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const AUTH_API : string = 'https://localhost:44377/api/auth/';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private httpClient: HttpClient) { }
-  login(){
-    
+  constructor(private http: HttpClient) { }
+  login(credentials): Observable<any> {
+    return this.http.post(`${AUTH_API}login`, {
+      username: credentials.username,
+      password: credentials.password
+    }, httpOptions);
+  }
+
+  register(user): Observable<any> {
+    return this.http.post(`${AUTH_API}register`, {
+      username: user.username,
+      email: user.email,
+      password: user.password
+    }, httpOptions);
   }
 }
